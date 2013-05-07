7edbf972493bb7181d01de8db3c3dc2758966540
xuanaiwu <xuanaiwu@gmail.com>
2013-05-07 20:38:54 +0800

    sql

---
+++ 数据库技术/Start sql.bat
@@ -0,0 +1,6 @@
+net start MSSQLSERVER
+net start MSSQL$SQLEXPRESS
+net start MSSQLServerOLAPService
+net start SQLBROWSER
+net start msftesql
+net start SQL Server VSS Writer
---
+++ 数据库技术/Stop sql.bat
@@ -0,0 +1,6 @@
+net stop MSSQLSERVER
+net stop MSSQL$SQLEXPRESS
+net stop MSSQLServerOLAPService
+net stop SQLBROWSER
+net stop msftesql
+net stop SQL Server VSS Writer
